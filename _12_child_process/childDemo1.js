548b43077e4c16ba8048cd060789453aee2bdde2
aidan200 <372944573@qq.com>
2017-06-12 18:01:44 +0800

    fourth

--- _12_child_process/childDemo1.js
+++ _12_child_process/childDemo1.js
@@ -5,14 +5,14 @@
 //argv对应父进程传递的所有值
 //argv[0]=执行模块的node.exe argv[1]=执行的模块 argv[2]=第一个参数
 console.log('进程'+process.argv[2]+'执行');
-/*
+
 //向父进程发送消息 fork模式
 process.send({mm:"你妹的"});
-//接受父进程的消息
+//接受父进程的消息(因为监听事件,子线程不退出)
 process.on('message',function (msg) {
     console.log("CHILD got message:",msg);
 })
-*/
+
 
 
 console.log("aaaaaaa123");
